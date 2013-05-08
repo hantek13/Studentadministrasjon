d66bcb034e63367f180d98707adfa640b6178b43
hantek13 <s188093@stud.hioa.no>
2013-05-08 15:11:12 +0300

    komentert Eksamen.java

--- Eksamen.java
+++ Eksamen.java
@@ -1,30 +1,47 @@
-package prosjekt;
+
 
 /**
- *
+ *Fil: Eksamen.java - klasse for eksamen
  * Skrevet av: Roza
  * Sist endret: 
  * Endret av: 
  */
+package prosjekt;
+
+/**
+* Klasse for eksamen
+*/
 public class Eksamen {
     private String dato;
     Eksamen neste;
     
+   /**
+    * Konstruktør
+    * @param d dato for når eksamen ble utgitt
+    */
     public Eksamen(String d) {
         dato = d;
     }
     
+    //default-konstruktør
     public Eksamen() {
         
     }
     
+   /**
+    * Henter eksamens dato
+    * @return dato
+    */
     public String getDato() {
         return dato;
     }
     
-    @Override
+   /**
+    * Skriver informasjon om eksamen
+    * $return Strengen med informasjon
+    */
     public String toString() {
         return "\nDato: " + dato;
     }
     
-}
+}//End of class Eksamen
